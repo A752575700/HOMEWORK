9a7366a253357beb6e467d0f868031ae6daf228d
A752575700 <752575700@qq.com>
2015-08-15 17:43:30 +0800

    ^ ^

--- Day5_2/Day5_2.playground/Contents.swift
+++ Day5_2/Day5_2.playground/Contents.swift
@@ -1,8 +1,8 @@
 //: Playground - noun: a place where people can play
 
-import Cocoa
+//import Cocoa
 
-var str = "Hello, playground"
+//var str = "Hello, playground"
 public class recursion{
 
     public func PrintNumber(n: Int){
@@ -25,9 +25,73 @@ public class recursion{
         }
         return items[index-1]
     }
+    public func jiecheng (number: Int) -> Int{
+        if number > 1{
+        return    jiecheng(number-1)*number
+        }
+        return 1
+    }
+    public func chengfang(a: Int, b: Int) -> Int {
+        if b > 1{
+            return chengfang(a, b:b - 1) * a
+        }
+        return a
+    }
+    public func absoluteValue (a: Int) -> Int{
+        if a > 0{
+            return a
+        }
+        else{
+            return 0 - a
+        }
+    }
+    
+    public func ZuiDaGongYueShu(a: Int, b: Int) -> Int{
+        
+        if a > b {
+            return ZuiDaGongYueShu(a - b, b: b)
+        }
+        else if a < b {
+            return ZuiDaGongYueShu(a, b: b-a)
+        }
+        else {
+            return a
+        }
+    }
+    public func solveHanuota(number: Int) -> Int{
+        if number > 1{
+            return solveHanuota(number-1) * 2 + 1
+        }
+        return 1
+        //return chengfang(2, n)-1
+    }
+    public func everyStep(number: Int, from: String, buffer: String, to: String) {
+        //var outcome: String = " "
+        if number == 1 {
+            println("move disk" + "1" + " from " + from  + " to " + to +  " throw " + buffer)
+            //return 1
+        }else{
+            everyStep(number - 1, from: from, buffer: to, to: buffer)
+            //println("move disk" + "\(number)" + " from A to C throw B" )
+            println("move disk" + "\(number)" + " from " + from  + " to " + to +  " throw " + buffer)
+            //return solveHanuota(number-1) * 2 + 1
+            everyStep(number - 1,from: buffer , buffer: from , to: to )
+        }
+        
+    }
 }
 
-
 var Recur = recursion()
-Recur.PrintNumber(4)
-Recur.biggerFibonaqi(92)
+//Recur.PrintNumber(4)
+//Recur.biggerFibonaqi(92)
+//Recur.jiecheng(0)
+println(Recur.chengfang(3,b: 4))
+
+println(Recur.ZuiDaGongYueShu(99, b:9 ))
+println(Recur.solveHanuota(5))
+
+//HW:every step
+
+
+Recur.everyStep(5, from: "A", buffer: "B" , to: "C")
+
